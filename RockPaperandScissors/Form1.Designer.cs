2d6718db454bc72abb55056c204de48c7c635810
Sahil Kajani <sahilkajani30@gmail.com>
2017-09-04 22:17:41 +0100

    Importing project

---
+++ RockPaperandScissors/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace RockPaperandScissors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rock = new System.Windows.Forms.PictureBox();
+            this.scissors = new System.Windows.Forms.PictureBox();
+            this.paper = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.displayImage = new System.Windows.Forms.PictureBox();
+            this.resultImage = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.drawScore = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.rock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scissors)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paper)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.displayImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rock
+            // 
+            this.rock.Image = ((System.Drawing.Image)(resources.GetObject("rock.Image")));
+            this.rock.Location = new System.Drawing.Point(21, 133);
+            this.rock.Name = "rock";
+            this.rock.Size = new System.Drawing.Size(129, 109);
+            this.rock.TabIndex = 0;
+            this.rock.TabStop = false;
+            this.rock.Click += new System.EventHandler(this.rock_Click);
+            // 
+            // scissors
+            // 
+            this.scissors.Image = ((System.Drawing.Image)(resources.GetObject("scissors.Image")));
+            this.scissors.Location = new System.Drawing.Point(481, 133);
+            this.scissors.Name = "scissors";
+            this.scissors.Size = new System.Drawing.Size(129, 109);
+            this.scissors.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.scissors.TabIndex = 1;
+            this.scissors.TabStop = false;
+            this.scissors.Click += new System.EventHandler(this.scissors_click);
+            // 
+            // paper
+            // 
+            this.paper.Image = ((System.Drawing.Image)(resources.GetObject("paper.Image")));
+            this.paper.Location = new System.Drawing.Point(258, 133);
+            this.paper.Name = "paper";
+            this.paper.Size = new System.Drawing.Size(129, 109);
+            this.paper.TabIndex = 2;
+            this.paper.TabStop = false;
+            this.paper.Click += new System.EventHandler(this.paper_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monospac821 BT", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(620, 51);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Rock Paper and Scissors";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monospac821 BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(550, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Please select image from below and click Play";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // displayImage
+            // 
+            this.displayImage.Location = new System.Drawing.Point(101, 276);
+            this.displayImage.Name = "displayImage";
+            this.displayImage.Size = new System.Drawing.Size(129, 109);
+            this.displayImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.displayImage.TabIndex = 5;
+            this.displayImage.TabStop = false;
+            this.displayImage.Click += new System.EventHandler(this.resultImage_Click);
+            // 
+            // resultImage
+            // 
+            this.resultImage.Location = new System.Drawing.Point(374, 276);
+            this.resultImage.Name = "resultImage";
+            this.resultImage.Size = new System.Drawing.Size(129, 109);
+            this.resultImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.resultImage.TabIndex = 6;
+            this.resultImage.TabStop = false;
+            this.resultImage.Click += new System.EventHandler(this.displayImage_Click);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Monospac821 BT", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(749, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 29);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Score";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(772, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Won";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(772, 321);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Draw";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(772, 225);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Lost";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(258, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.play_click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(285, 436);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(775, 367);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 17);
+            this.label9.TabIndex = 15;
+            // 
+            // drawScore
+            // 
+            this.drawScore.AutoSize = true;
+            this.drawScore.Location = new System.Drawing.Point(772, 350);
+            this.drawScore.Name = "drawScore";
+            this.drawScore.Size = new System.Drawing.Size(48, 17);
+            this.drawScore.TabIndex = 16;
+            this.drawScore.Text = "          ";
+            this.drawScore.Click += new System.EventHandler(this.drawScore_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(766, 258);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 17);
+            this.label10.TabIndex = 17;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(766, 171);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 17);
+            this.label11.TabIndex = 18;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(778, 171);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(100, 17);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "                       ";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(766, 258);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 17);
+            this.label13.TabIndex = 20;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(778, 257);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(92, 17);
+            this.label14.TabIndex = 21;
+            this.label14.Text = "                     ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 483);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.drawScore);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resultImage);
+            this.Controls.Add(this.displayImage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.paper);
+            this.Controls.Add(this.scissors);
+            this.Controls.Add(this.rock);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.rock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scissors)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paper)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.displayImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox rock;
+        private System.Windows.Forms.PictureBox scissors;
+        private System.Windows.Forms.PictureBox paper;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox displayImage;
+        private System.Windows.Forms.PictureBox resultImage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label drawScore;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
---
+++ RockPaperandScissors/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockPaperandScissors
+{
+
+    public partial class Form1 : Form
+    {
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rock_Click(object sender, EventArgs e)
+        {
+            bool myimage = true;
+            if (myimage)
+            {
+                displayImage.Image = rock.Image;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resultImage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void paper_Click(object sender, EventArgs e)
+        {
+            bool myimage = true;
+            if (myimage)
+            {
+                displayImage.Image = paper.Image;
+            }
+        }
+
+        private void scissors_click(object sender, EventArgs e)
+        {
+            bool myimage = true;
+            if (myimage)
+            {
+                displayImage.Image = scissors.Image;
+            }
+        }
+
+        private void displayImage_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        public void play_click(object sender, EventArgs e)
+        {
+            Random myPicture = new Random();
+            int i = myPicture.Next(1, 4);
+
+            if (displayImage.Image == null)
+            {
+                MessageBox.Show("Please select an Image", "Image Not Selected");
+                return;
+            }
+
+
+
+            if (i == 1)
+                resultImage.Image = rock.Image;
+            else if (i == 2)
+                resultImage.Image = paper.Image;
+            else if (i == 3)
+                resultImage.Image = scissors.Image;
+
+            #region Draw Conditions
+
+            //Setting Draw conditions 
+            if (resultImage.Image == rock.Image && displayImage.Image == rock.Image)
+            {
+                MessageBox.Show("It's a Draw", "Draw!");
+                myResult.Draw++;
+                drawScore.Text = myResult.Draw.ToString();
+                
+            }
+
+            else if (resultImage.Image == paper.Image && displayImage.Image == paper.Image)
+            {
+                MessageBox.Show("It's a Draw", "Draw!");
+                myResult.Draw++;
+                drawScore.Text = myResult.Draw.ToString();
+            }
+            else if (resultImage.Image == scissors.Image && displayImage.Image == scissors.Image)
+            {
+                MessageBox.Show("It's a Draw", "Draw!");
+                myResult.Draw++;
+                drawScore.Text = myResult.Draw.ToString();
+            }
+
+            #endregion
+
+            #region Rock
+            //Setting conditions for rock
+            if (displayImage.Image == rock.Image && resultImage.Image == paper.Image)
+            {
+                MessageBox.Show("You Loose", "Sorry!");
+                myResult.Lost++;
+                label14.Text = myResult.Lost.ToString();
+            }
+            else if (displayImage.Image == rock.Image && resultImage.Image == scissors.Image)
+            {
+                MessageBox.Show("You Won", "Congrats!");
+                myResult.Won++;
+                label12.Text = myResult.Won.ToString();
+            }
+            #endregion
+
+
+            #region Paper
+            //Setting conditions for paper
+            if (displayImage.Image == paper.Image && resultImage.Image == rock.Image)
+            {
+                MessageBox.Show("You Won", "Congrats!");
+                myResult.Won++;
+                label12.Text = myResult.Won.ToString();
+            }
+            else if (displayImage.Image == paper.Image && resultImage.Image == scissors.Image)
+            {
+                MessageBox.Show("You Loose", "Sorry!");
+                myResult.Lost++;
+                label14.Text = myResult.Lost.ToString();
+            }
+
+
+            #endregion
+
+
+            #region scissors
+            //Setting conditions for scissors
+            if (displayImage.Image == scissors.Image && resultImage.Image == rock.Image)
+            {
+                MessageBox.Show("You Loose", "Sorry!");
+                myResult.Lost++;
+                label14.Text = myResult.Lost.ToString();
+            }
+            else if (displayImage.Image == scissors.Image && resultImage.Image == paper.Image)
+            {
+                MessageBox.Show("You Won", "Congrats!");
+                myResult.Won++;
+                label12.Text = myResult.Won.ToString();
+            }
+
+
+#endregion
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void resultLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public class myResult
+        {
+            public static int Won { get; set; }
+            public static int Lost { get; set; }
+            public static int Draw { get; set; }
+        }
+
+        private void drawScore_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
